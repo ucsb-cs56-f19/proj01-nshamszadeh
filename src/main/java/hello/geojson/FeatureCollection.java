8bdde65e60da12df48900ed50dde0c08b3fcf3d2
Navid Shamszadeh <nshamszadeh@gmail.com>
2019-11-20 16:33:55 -0800

    added metadata object and updated results.html to display url and count

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -9,6 +9,7 @@ public class FeatureCollection {
   private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
 
   public String type;
+  public Metadata metadata;
 
     /**
      * Create a FeatureCollection object from json representation
