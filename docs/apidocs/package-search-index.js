ef4452f2d8519f8dab3f322da0bfbaa99eee0c98
Navid Shamszadeh <nshamszadeh@gmail.com>
2019-12-02 23:59:45 -0800

    ns - finished refactoring code

--- docs/apidocs/package-search-index.js
+++ docs/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hello"},{"l":"hello.controllers"},{"l":"hello.entities"},{"l":"hello.geojson"},{"l":"hello.repositories"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hello"},{"l":"hello.controllers"},{"l":"hello.entities"},{"l":"hello.geojson"},{"l":"hello.repositories"},{"l":"hello.searches"},{"l":"hello.services"}]
